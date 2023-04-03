562839c304e18fb4de134db4c8192bf410fe59dc
brgandluru <srisikharam@gmail.com>
2023-04-03 12:41:28 -0500

    commiting bank-congif

---
+++ bank-config-server/src/main/java/com/arka/bankconfigserver/BankConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.arka.bankconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankConfigServerApplication.class, args);
+    }
+
+}
